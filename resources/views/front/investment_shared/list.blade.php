5a00abbaeeb122deb79a2e80c53c22f28a8218e7
Jacek <kontakt@jk-design.pl>
2023-04-07 14:42:45 +0200

    RWD part 4

--- resources/views/front/investment_shared/list.blade.php
+++ resources/views/front/investment_shared/list.blade.php
@@ -1,17 +1,17 @@
 <div id="roomsList">
-    <div class="container">
+    <div class="container p-0">
         @if($properties->count() > 0)
             @foreach($properties as $room)
-                <div class="row">
+                <div class="row m-0">
                     @if($room->price)
                         <span class="ribbon1"><span>Oferta specjalna</span></span>
                     @endif
-                    <div class="col col-top">
+                    <div class="col-12 col-md col-top p-0 d-flex align-items-center justify-content-center justify-content-md-start">
                         <a href="{{route('front.investment.property.index', ['slug' => $investment->slug, 'floor' => $room->floor_id, 'property' => $room->id])}}">
                             <h2>{{$room->name_list}}</h2>
                         </a>
                     </div>
-                    <div class="col">
+                    <div class="col-6 col-md justify-content-center">
                         @if($room->file)
                             <picture>
                                 <source type="image/webp" srcset="/investment/property/list/webp/{{$room->file_webp}}">
@@ -20,7 +20,7 @@
                             </picture>
                         @endif
                     </div>
-                    <div class="col">
+                    <div class="col-6 col-md pe-3 pe-lg-5">
                         <ul class="mb-0 list-unstyled">
                             @if($room->price)
                                 <li>cena: <b>@money($room->price)</b></li>
@@ -32,7 +32,7 @@
                     <div class="col justify-content-center">
                         <span class="badge room-list-status-{{ $room->status }}">{{ roomStatus($room->status) }}</span>
                     </div>
-                    <div class="col justify-content-end col-list-btn">
+                    <div class="col justify-content-end col-list-btn p-0">
                         <a href="{{route('front.investment.property.index', ['slug' => $investment->slug, 'floor' => $room->floor_id, 'property' => $room->id])}}" class="bttn">ZOBACZ</a>
                     </div>
                 </div>
