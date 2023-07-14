a0340bad975b83318f1b24f26a97659b7bdb0462
Jacek <kontakt@jk-design.pl>
2023-07-14 10:18:03 +0200

    Poprawa walidacji formularzy

--- resources/views/front/investment_property/index.blade.php
+++ resources/views/front/investment_property/index.blade.php
@@ -133,7 +133,7 @@
                                             @foreach ($rules as $r)
                                                 <div class="col-12 @error('rule_'.$r->id) is-invalid @enderror">
                                                     <div class="rodo-rule clearfix">
-                                                        <input name="rule_{{$r->id}}" id="zgoda_{{$r->id}}" value="1" type="checkbox" @if($r->required === 1) class="validate[required]" @endif data-prompt-position="topLeft:0">
+                                                        <input name="rule_{{$r->id}}" id="rule_{{$r->id}}" value="1" type="checkbox" @if($r->required === 1) class="validate[required]" @endif data-prompt-position="topLeft:0">
                                                         <label for="rule_{{$r->id}}" class="rules-text">
                                                             {!! $r->text !!}
                                                             @error('rule_'.$r->id)
